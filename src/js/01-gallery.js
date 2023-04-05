209685976198b1f895c5accd1d6586c13aeb0fc8
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-04-05 12:28:22 +0200

    task#1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+for (const image of galleryItems) {
+  const listEl = `<li class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+     <img class="gallery__image" src=${image.preview} alt=${image.description} />
+  </a>
+</li>`;
+  galleryEl.insertAdjacentHTML('beforeend', listEl);
+
+  galleryEl.addEventListener('click', event => event.preventDefault());
+}
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
